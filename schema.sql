6261624ff1a850aa2d2c478f79aa931413f863d6
Costa-LG <costalgaf@gmail.com>
2025-05-21 14:42:15 -0300

    Python Cleanup v2.5

--- schema.sql
+++ schema.sql
@@ -2,35 +2,26 @@
 CREATE DATABASE IF NOT EXISTS `imdb`;
 USE `imdb`;
 
--- RESETING TABLE
+-- RESETING TABLES
 DROP TABLE IF EXISTS `movies`;
-DROP VIEW IF EXISTS `top_10_gross_income`;
+DROP TABLE IF EXISTS `actors`;
+DROP TABLE IF EXISTS `acted`;
+DROP TABLE IF EXISTS `directors`;
+DROP TABLE IF EXISTS `directed`;
+DROP TABLE IF EXISTS `plot`;
 
--- CREATING THE `imdb_top_1000` TABLE
-CREATE TABLE `imdb_top_1000` (
-    `id` INT UNSIGNED AUTO_INCREMENT,
-    `poster_link` TEXT,
-    `series_title` VARCHAR(100),
-    `released_year` YEAR DEFAULT NULL,
+-- CREATING TABLES
+CREATE TABLE `movies` (
+    `id` INT AUTO_INCREMENT,
+    `title` VARCHAR(100),
+    `released_year` YEAR,
     `certificate` VARCHAR(10), -- ENUM('','')
-    `runtime` VARCHAR(10),
-    `genre` VARCHAR(50), -- SET('','')
-    `IMDB_rating` DECIMAL(5,2) NOT NULL,
-    `overview` TEXT,
-    `meta_score` INT NULL,
-    `director` VARCHAR(50),
-    `star1` VARCHAR(50),
-    `star2` VARCHAR(50),
-    `star3` VARCHAR(50),
-    `star4` VARCHAR(50),
-    `votes` INT UNSIGNED,
-    `gross` TEXT,
+    `runtime_in_minutes` INT,
+    `genre` VARCHAR(100), -- SET('','')
+    `IMDB_rating` DECIMAL(2,1),
+    `meta_score` INT,
+    `votes` INT,
+    `gross` INT,
     PRIMARY KEY (`id`)
 );
-
-DESCRIBE `imdb_top_1000`;
--- CREATE VIEW `top_10_gross_income` AS
--- SELECT `series_title`, `director`, `star1`, `star2`, `star3`, `gross` FROM `imdb_top_1000`
--- ORDER BY `gross` DESC
--- LIMIT 10;
 
